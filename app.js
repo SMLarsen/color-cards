9b90ac45b11097abb5737b92804576bb251ff9f2
Steve Larsen <stevelarsen01@gmail.com>
2016-11-08 06:56:26 -0600

    card sequencing working

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var card = {};
 var inputArray = [];
 var cardArray = [];
 var position = 0;
+var interval;
+var currentCard = 0;
+var totalCards = 0;
 
 function Card(position, color, timerSeconds, showTimer) {
   this.position = position;
@@ -10,48 +13,97 @@ function Card(position, color, timerSeconds, showTimer) {
   this.showTimer = showTimer;
 }
 
+// var card = new Card(0, 'orange', 2, false);
+// cardArray.push(card);
+// var card = new Card(0, 'red', 2, false);
+// cardArray.push(card);
+// var card = new Card(0, 'blue', 2, false);
+// cardArray.push(card);
+// var card = new Card(0, 'green', 2, false);
+// cardArray.push(card);
+
 $(document).ready(function() {
+//-------------------
+  $("#start-btn").on("click", function(event) {
+    event.preventDefault();
+    startBtn();
+  });
+
+  $("#stop-btn").on("click", function(event) {
+    event.preventDefault();
+    stopBtn();
+  });
+  //-------------------
+
 
   $("#add-button").on("click", function(event) {
     event.preventDefault();
     addCard();
   });
 
-function addCard() {
-  inputArray = $(".config-form").serializeArray();
-  $.each(inputArray, function(i, field){
-    $("#results").append(field.name + ":" + field.value + " ");
+  $("#start-button").on("click", function(event) {
+    event.preventDefault();
+    startDisplay();
   });
-  var color = '';
-  var timerSeconds = 0;
-  var showTimer = false;
-  cardValues = {};
-  inputArray.forEach(function (element, index, array) {
-    cardValues[element.name] = element.value;
-  });
-  position++;
-  color = cardValues.color;
-  timerSeconds = cardValues.duration;
-  showTimer = false;
-  if (cardValues.countdown === 'true') {
-    showTimer = true;
+
+  function addCard() {
+    inputArray = $(".config-form").serializeArray();
+
+    var color = '';
+    var timerSeconds = 0;
+    var showTimer = false;
+    cardValues = {};
+
+    $.each(inputArray, function(i, field){
+      $("#results").append(field.name + ":" + field.value + " ");
+    });
+
+    inputArray.forEach(function (element, index, array) {
+      cardValues[element.name] = element.value;
+    });
+
+    position++;
+    color = cardValues.color;
+    timerSeconds = cardValues.duration;
+    showTimer = false;
+    if (cardValues.countdown === 'true') {
+      showTimer = true;
+    }
+
+    card = new Card(position, color, timerSeconds, showTimer);
+    cardArray.push(card);
+
+    totalCards = cardArray.length;
+
+    displayCard(card);
   }
-  card = new Card(position, color, timerSeconds, showTimer);
-  cardArray.push(card);
-  displayCard(card);
-  console.log(cardArray);
-}
 
-function displayCard (card) {
-  var string = '<tr class="cardRow">';
-  string += '<td>' + card.position + '</td>';
-  string += '<td>' + card.color + '</td>';
-  string += '<td>' + card.timerSeconds + '</td>';
-  string += '<td>' + card.showTimer + '</td>';
-  string += '</tr>';
-  $('#card-table').append(string);
-  console.log(1234);
-}
+  function displayCard (card) {
+    var string = '<tr class="cardRow">';
+    string += '<td>' + card.position + '</td>';
+    string += '<td>' + card.color + '</td>';
+    string += '<td>' + card.timerSeconds + '</td>';
+    string += '<td>' + card.showTimer + '</td>';
+    string += '</tr>';
+    $('#card-table').append(string);
+  }
 
+  function startDisplay() {
+    card = cardArray[currentCard];
+    currentCard++;
+    $('.config-container').css("visibility", "hidden");
+    $('body').css("background-color", card.color);
+    setTimeout(setCardCSS, card.timerSeconds * 1000);
+  }
 
+  function setCardCSS() {
+    if (currentCard < totalCards) {
+      card = cardArray[currentCard];
+      currentCard++;
+      $('body').css("background-color", card.color);
+      setTimeout(setCardCSS, card.timerSeconds * 1000);
+    } else {
+      $('body').css("background-color", 'white');
+    }
+  }
 });
